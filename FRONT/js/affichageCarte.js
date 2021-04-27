f62e2efc06e01275d67d0208976a2338cf680c04
oceane <oceane.guyot.mentorat@gmail.com>
2021-04-27 22:59:08 +0200

    Affichage correct du jeu

--- FRONT/js/affichageCarte.js
+++ FRONT/js/affichageCarte.js
@@ -1,9 +1,7 @@
 //Le but ici est d'afficher les cartes dans la même logique que la génération du css
 
 var nbPaires = 18;
-
 var divAffichageCarte = $('#cartes');
-
 // On génère un tableau avec tous les ids générés en sass
 var tabData = new Array();
 for(let i=1; i<=nbPaires; i++){
@@ -11,17 +9,32 @@ for(let i=1; i<=nbPaires; i++){
         tabData.push("carte-"+j+i);
     }
 }
-
 tabData = shuffle(tabData); //On randomise les ids pour pouvoir les "mélanger" pour l'afficher sur la page
 
+//On boucle sur le tableau pour afficher les cartes
 tabData.forEach(data=>{
-    $('#cartes').append("<div id="+data+"></div>");
+
+    let $parentCarte = document.createElement('div'); //parent qui va contenir le recto et le verso
+    $($parentCarte).addClass('carte')
+
+    //Création du recto de la carte - face cachée
+    let $recto = document.createElement('div');
+    $($recto).addClass('carte-recto');
+
+    //Creation du verso de la carte - face visible
+    let $divPhoto = document.createElement('div');
+    $divPhoto.id = data;
+    $($divPhoto).addClass('carte-verso');
+    $($divPhoto).hide(); //Par défaut la carte est caché
+
+    // On ajoute les 2 cartes a la div commune
+    $($parentCarte).append($recto);
+    $($parentCarte).append($divPhoto);
+
+    divAffichageCarte.append($parentCarte) //On affecte la carte a la div parent dans le HTML
 })
 
 
-
-console.log(tabData);
-// console.log(shuffle(tabData));
 
 
 //Fonction pour mélanger un tableau

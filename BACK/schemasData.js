39d845c75aff214c29137d6ce766d96c48205d57
oceane <oceane.guyot.mentorat@gmail.com>
2021-04-30 08:47:37 +0200

    ajout d'une précision sur le type de données en commentaire

--- BACK/schemasData.js
+++ BACK/schemasData.js
@@ -1,7 +1,7 @@
 /**
  * Schémas de données pour la sauvegarde en bdd
  * Pour chaque champs, on va déterminer le type de donnée attendu, ainsi que si le champs est obligatoire ou non. 
- * Dans notre cas, nous avons besoin de deux champs (qui sont obligatoire), le pseudo de la personne qui a joué, ainsi que le temps qu'il a mis pour gagner.
+ * Dans notre cas, nous avons besoin de deux champs (qui sont obligatoire), le pseudo de la personne qui a joué, ainsi que le temps qu'il a mis pour gagner (qui sera sauvegardé en type nombre, car sauvegardé en secondes).
  * + vient se rajouter un id qui sera généré automatiquement, donc pas besoin de le préciser dans le schemas de données
  */
 const mongoose = require("mongoose");

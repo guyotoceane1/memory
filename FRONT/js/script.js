6ba9c57a448b9fda02b3a3e2e34b5c07a064118f
oceane <oceane.guyot.mentorat@gmail.com>
2021-05-01 22:48:42 +0200

    affichage des scores sur la page d'accueil

--- FRONT/js/script.js
+++ FRONT/js/script.js
@@ -18,10 +18,39 @@ $.ajax({
     type: "GET",
     url: "http://localhost:3000/",
     success: function(response){
-        console.log(response)
+        traitementData(response)
     },
     error : function(error){
         console.log(error)
     },
     dataType: 'json',
   })
+  
+
+  function traitementData(data){
+      let i=1;
+      data.forEach(element => {
+          console.log(element)
+          let $tr = document.createElement('tr');
+          
+          $tr.appendChild(createTD(i));
+          $tr.appendChild(createTD(element.pseudo));
+          $tr.appendChild(createTD(element.temps));
+          $tr.appendChild(createTD(element.difficulte));
+
+          if(i%2 !== 0){
+            $($tr).css("background-color","orange");
+            $($tr).css("color","white");
+          }
+
+          $('#tbodyScore').append($tr);
+
+          i++;
+      });
+  }
+
+  function createTD(value){
+    let $td = document.createElement('td');
+    $td.appendChild(document.createTextNode(value));
+    return $td;
+  }
